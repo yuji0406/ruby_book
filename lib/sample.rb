a53dadb48e591306405ee3443deafd4f83c02c1d
yuji0406 <ky19930305@gmail.com>
2022-01-03 19:20:24 +0900

    例外処理　retry,例外オブジェクトのメソッド

--- lib/sample.rb
+++ lib/sample.rb
@@ -1,14 +1,9 @@
-puts 'Start.'
-module Greeter
-  def hello
-    'hello'
-  end
-end
-
 begin
-  greeter = Greeter.new
-rescue
-  puts '例外が発生したが、このまま続行する'
+  1 / 0
+rescue => e
+  puts "エラークラス: #{e.class}"
+  puts "エラーメッセージ: #{e.message}"
+  puts "バックトレース ----"
+  puts e.backtrace
+  puts "----"
 end
-
-puts 'End.'
