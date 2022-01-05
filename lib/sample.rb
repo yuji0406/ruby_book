623b2b504ea5166b77db2bc3132f2ee11a8924ca
yuji0406 <ky19930305@gmail.com>
2022-01-05 15:33:22 +0900

    西暦変換プログラム

--- lib/sample.rb
+++ lib/sample.rb
@@ -1,16 +1,17 @@
-def send_mail_to(user)
-  if user
-  puts "#{user}にメールを送信しました"
-  1 / 0
-  end
-end
-users = ["satou", "bob", "alice"]
+require 'date'
 
-users.each do |user|
+def convert_heisei_to_date(heisei_text)
+  m = heisei_text.match(/平成(?<jp_year>\d+)年(?<month>\d+)月(?<day>\d+)日/)
+  year = m[:jp_year].to_i + 1988
+  month = m[:month].to_i
+  day = m[:day].to_i
   begin
-    send_mail_to(user)
-  rescue => e
-    puts "#{e.class}: エラーメッセージ#{e.message}"
-    puts e.backtrace
+    p Date.new(year, month, day)
+  rescue ArgumentError
+    nil
   end
 end
+
+convert_heisei_to_date('平成28年12月31日')
+convert_heisei_to_date('平成29年88月22日')
+convert_heisei_to_date('平成1年11月2日')
