d6ed1fc91eb44c1753476787328aadb2feffa703
yuji0406 <ky19930305@gmail.com>
2022-02-01 09:26:33 +0900

    Procオブジェクト

---
+++ lib/word_synth.rb
@@ -0,0 +1,18 @@
+class WordSynth
+
+# include 'Effects'
+
+  def initialize 
+    @effects = []  
+  end
+
+  def add_efffect(effect)
+    @effects << effect
+  end
+
+  def play(original_words)
+    @effects.inject(original_words) do |words, effect|
+      effect.call(words)
+    end
+  end
+end
